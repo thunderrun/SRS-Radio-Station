5f9be0587e4db7d487c6238a4e2ace7a2eaa18d8
Toumatsu Mimi <thunderrun2035@gmail.com>
2020-09-22 16:30:42 +0800

    Create radioStation.bat

---
+++ radioStation.bat
@@ -0,0 +1,8 @@
+@echo off
+title Radio Station Master
+:Restart
+for /r %%f in (*.mp3) do (
+echo Now Playing %%~nxf
+start "Radio Station" /wait "DCS-SR-ExternalAudio.exe" "%%~nxf" 305.0 AM 2 5002 RadioStation 1.0
+)
+goto Restart
