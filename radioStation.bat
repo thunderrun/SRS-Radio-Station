a63b80df5c6c9a552d6c9e49ad70a658f5fcc943
Toumatsu Mimi <thunderrun2035@gmail.com>
2020-09-22 17:20:23 +0800

    Update radioStation.bat

--- radioStation.bat
+++ radioStation.bat
@@ -3,6 +3,6 @@ title Radio Station Master
 :Restart
 for /r %%f in (*.mp3) do (
 echo Now Playing %%~nxf
-start "Radio Station" /wait "DCS-SR-ExternalAudio.exe" "%%~nxf" 305.0 AM 2 5002 RadioStation 1.0
+start "Radio Station" /wait "DCS-SR-ExternalAudio.exe" "%%f" 305.0 AM 2 5002 RadioStation 1.0
 )
 goto Restart
